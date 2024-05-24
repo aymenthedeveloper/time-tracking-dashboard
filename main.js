c85b5e69eb7930e9c7c1c9a1bc4b8d16e6aabf5d
aymenthedeveloper <aymendev.contact@gmail.com>
2024-05-24 11:58:31 +0100

    added media queries and finished mobile version

---
+++ main.js
@@ -0,0 +1,128 @@
+const data = [
+  {
+    "title": "Work",
+    "timeframes": {
+      "daily": {
+        "current": 5,
+        "previous": 7
+      },
+      "weekly": {
+        "current": 32,
+        "previous": 36
+      },
+      "monthly": {
+        "current": 103,
+        "previous": 128
+      }
+    }
+  },
+  {
+    "title": "Play",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 2
+      },
+      "weekly": {
+        "current": 10,
+        "previous": 8
+      },
+      "monthly": {
+        "current": 23,
+        "previous": 29
+      }
+    }
+  },
+  {
+    "title": "Study",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 7
+      },
+      "monthly": {
+        "current": 13,
+        "previous": 19
+      }
+    }
+  },
+  {
+    "title": "Exercise",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 5
+      },
+      "monthly": {
+        "current": 11,
+        "previous": 18
+      }
+    }
+  },
+  {
+    "title": "Social",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 3
+      },
+      "weekly": {
+        "current": 5,
+        "previous": 10
+      },
+      "monthly": {
+        "current": 21,
+        "previous": 23
+      }
+    }
+  },
+  {
+    "title": "Self Care",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 2,
+        "previous": 2
+      },
+      "monthly": {
+        "current": 7,
+        "previous": 11
+      }
+    }
+  }
+];
+const dataContainer = document.querySelectorAll('.time-tracker .card .details .data');
+function updateCards (filter='weekly'){
+for (let i = 0; i < dataContainer.length; i++) {
+  const e = data[i];
+  const container = dataContainer[i];
+  container.innerHTML = `
+    <p class="current">${e.timeframes[filter].current}hrs</p>
+    <p class="previous">Last ${filter==='daily'?'day': filter==='weekly'?'week': 'month'} - ${e.timeframes[filter].previous}hrs</p>
+    `
+  }
+}
+
+const btns = document.querySelectorAll('.filters button');
+const clearActive = () => btns.forEach(btn => btn.classList.remove('active'))
+btns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    clearActive();
+    btn.classList.add('active');
+    updateCards(btn.dataset.filter);
+  })
+});
+
+
+updateCards()
